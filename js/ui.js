2756e1aa3e6785798ee5d6471de051b8d49fc8aa
Jagvir Dehal <jagvir@localhost.localdomain>
2020-05-01 09:48:06 -0400

    Implemented display (right panel) updates

--- js/ui.js
+++ js/ui.js
@@ -18,7 +18,9 @@ function addVector() {
   vector.setAttribute('location', `${x} ${y} ${z}`);
   vector.setAttribute('id', `${id}`);
 
-  objects.push(id);
+  let newObject = {id: id, type: "vector"};
+
+  objects.push(newObject);
 
   localStorage.setItem("objectCount", objectCount + 1);
   localStorage.setItem("objects", JSON.stringify(objects));
@@ -50,7 +52,59 @@ function updateObject(event) {
 function updateDisplay() {
   let display = document.getElementById("display");
 
-  let item = document.createElement("div");
+  display.textContent = '';
 
-  display.appendChild()
+  let objects = JSON.parse(localStorage.getItem("objects"));
+  let objectCount = JSON.parse(localStorage.getItem("objectCount"));
+  
+  objects.forEach(element => {
+    console.log(element);
+    
+    let container = document.createElement("div");
+    let title = document.createElement("span");
+    let inputX = document.createElement("input");
+    let inputY = document.createElement("input");
+    let inputZ = document.createElement("input");
+
+    container.setAttribute("id", `${element.id}`);
+    container.setAttribute("class", "display-item");
+
+    title.innerHTML = element.id;
+
+    let location = document.querySelector(`a-vector3#${element.id}`).getAttribute('location').split(' ');
+
+    inputX.setAttribute("type", "number");
+    inputX.setAttribute("name", "update_x");
+    inputX.setAttribute("id", `${element.id}`);
+    inputX.setAttribute("min", "-99");
+    inputX.setAttribute("max", "99");
+    inputX.setAttribute("placeholder", "X");
+    inputX.setAttribute("onchange", "updateObject(event)");
+    inputX.value = location[0];
+
+    inputY.setAttribute("type", "number");
+    inputY.setAttribute("name", "update_y");
+    inputY.setAttribute("id", `${element.id}`);
+    inputY.setAttribute("min", "-99");
+    inputY.setAttribute("max", "99");
+    inputY.setAttribute("placeholder", "Y");
+    inputY.setAttribute("onchange", "updateObject(event)");
+    inputY.value = location[1];
+    
+    inputZ.setAttribute("type", "number");
+    inputZ.setAttribute("name", "update_z");
+    inputZ.setAttribute("id", `${element.id}`);
+    inputZ.setAttribute("min", "-99");
+    inputZ.setAttribute("max", "99");
+    inputZ.setAttribute("placeholder", "Z");
+    inputZ.setAttribute("onchange", "updateObject(event)");
+    inputZ.value = location[2];
+
+    container.appendChild(title);
+    container.appendChild(inputX);
+    container.appendChild(inputY);
+    container.appendChild(inputZ);
+
+    display.appendChild(container);
+  });
 }
