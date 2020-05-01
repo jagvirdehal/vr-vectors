358cac6401fc14d5a702076a58d2f5d7a654f5e8
Jagvir Dehal <jagvir@localhost.localdomain>
2020-05-01 08:48:14 -0400

    Created new updateVector function and update event

--- js/vector.js
+++ js/vector.js
@@ -36,61 +36,76 @@ AFRAME.registerComponent("vector3", {
     let body = document.createElement("a-cylinder");
     let head = document.createElement("a-cone");
 
-    // Calculate vector coordinates
-    let deltaX, deltaY, deltaZ;
-    if (this.data.delta) { // See delta schema
-      deltaX = this.data.location.x;
-      deltaY = this.data.location.y;
-      deltaZ = this.data.location.z;
-    } else {
-      deltaX = this.data.location.x - this.data.origin.x;
-      deltaY = this.data.location.y - this.data.origin.y;
-      deltaZ = this.data.location.z - this.data.origin.z;
-    }
-    
-    // Magnitude of vector
-    let size = Math.sqrt(deltaX ** 2 + deltaY ** 2 + deltaZ ** 2);
-
-    let coneRadius = this.data.radius * 2;
-    let coneHeight = coneRadius * 2;
-
-    // Settings for cylinder shape (line)
-    // Position offset by half of size so it starts at the origin
-    body.setAttribute("position", `0 ${(size - coneHeight) / 2} 0`);
-    body.setAttribute("radius", `${this.data.radius}`);
-    body.setAttribute("height", `${size - coneHeight}`);
-    body.setAttribute("color", this.data.color);
-
-    // Settings for cone shape (pointer)
-    head.setAttribute("position", `0 ${size - (coneHeight / 2)} 0`);
-    head.setAttribute("radius-bottom", `${coneRadius}`);
-    head.setAttribute("radius-top", `0`);
-    head.setAttribute("height", `${coneHeight}`);
-    head.setAttribute("color", this.data.color);
-
-    // Calculate X rotation using dot product from vector (0, 1, 0)
-    let rotX = (Math.acos(deltaY / size) * 180) / Math.PI;
-
-    // Calculate Y rotation using dot product between (X, 0, Z) and (0, 0, 1)
-    let sansY = Math.sqrt(deltaX ** 2 + deltaZ ** 2);
-    let rotY;
-
-    // Check if (X, 0, Z) === (0, 0, 0) to avoid division by zero
-    if (sansY === 0) {
-      rotY = 0;
-    } else {
-      rotY = (Math.acos(deltaZ / sansY) * 180) / Math.PI;
-    }
-
-    // Apply rotation and position attributes to wrapper
-    wrapper.setAttribute("rotation", `${rotX} ${rotY} 0`);
-    wrapper.setAttribute("position",
-      `${this.data.origin.x} ${this.data.origin.y} ${this.data.origin.z}`
-    );
+    updateVector(this.data, wrapper, head, body);
 
     // Add wrapped, body, and head elements to the vector3 component
     this.el.appendChild(wrapper);
     wrapper.appendChild(body);
     wrapper.appendChild(head);
   },
+
+  update: function() {
+    console.log(this);
+    let wrapper = this.el.children[0];
+    let body = wrapper.children[0];
+    let head = wrapper.children[1];
+
+    updateVector(this.data, wrapper, head, body);
+  }
 });
+
+function updateVector(data, wrapper, head, body) {
+  console.log(data);
+  
+  // Calculate vector coordinates
+  let deltaX, deltaY, deltaZ;
+  if (data.delta) { // See delta schema
+    deltaX = data.location.x;
+    deltaY = data.location.y;
+    deltaZ = data.location.z;
+  } else {
+    deltaX = data.location.x - data.origin.x;
+    deltaY = data.location.y - data.origin.y;
+    deltaZ = data.location.z - data.origin.z;
+  }
+  
+  // Magnitude of vector
+  let size = Math.sqrt(deltaX ** 2 + deltaY ** 2 + deltaZ ** 2);
+
+  let coneRadius = data.radius * 2;
+  let coneHeight = coneRadius * 2;
+
+  // Settings for cylinder shape (line)
+  // Position offset by half of size so it starts at the origin
+  body.setAttribute("position", `0 ${(size - coneHeight) / 2} 0`);
+  body.setAttribute("radius", `${data.radius}`);
+  body.setAttribute("height", `${size - coneHeight}`);
+  body.setAttribute("color", data.color);
+
+  // Settings for cone shape (pointer)
+  head.setAttribute("position", `0 ${size - (coneHeight / 2)} 0`);
+  head.setAttribute("radius-bottom", `${coneRadius}`);
+  head.setAttribute("radius-top", `0`);
+  head.setAttribute("height", `${coneHeight}`);
+  head.setAttribute("color", data.color);
+
+  // Calculate X rotation using dot product from vector (0, 1, 0)
+  let rotX = (Math.acos(deltaY / size) * 180) / Math.PI;
+
+  // Calculate Y rotation using dot product between (X, 0, Z) and (0, 0, 1)
+  let sansY = Math.sqrt(deltaX ** 2 + deltaZ ** 2);
+  let rotY;
+
+  // Check if (X, 0, Z) === (0, 0, 0) to avoid division by zero
+  if (deltaX === 0) {
+    rotY = 0;
+  } else {
+    rotY = (Math.abs(deltaX) / deltaX) * (Math.acos(deltaZ / sansY) * 180) / Math.PI;
+  }
+
+  // Apply rotation and position attributes to wrapper
+  wrapper.setAttribute("rotation", `${rotX} ${rotY} 0`);
+  wrapper.setAttribute("position",
+    `${data.origin.x} ${data.origin.y} ${data.origin.z}`
+  );
+}
