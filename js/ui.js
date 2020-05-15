fe2fd26cdedf7d70549fb8f386a74b630be47a3a
jagvirdehal <jagvird216@gmail.com>
2020-05-15 18:05:52 -0400

    Adjusted UI to work better with Mobile. Mobile compatibility is my new priority

--- js/ui.js
+++ js/ui.js
@@ -7,32 +7,24 @@ function init() {
 }
 
 // Add a vector to the scene
-// TODO: Adjust input methods
-//       Add more object details for storage
-function addVector() {
+// TODO: Add more object details for storage
+function addVectorObject(vectorId, x, y, z) {
   // Grab grid component and create a vector
   let grid = document.querySelector("a-grid");
   let vector = document.createElement("a-vector3");
 
   // Get objects from localStorage
   let objects = JSON.parse(localStorage.getItem("objects"));
-  let objectCount = parseInt(localStorage.getItem("objectCount"));
-
-  // Getting values from input boxes *TODO(1)
-  let x = document.getElementsByName('vector_x')[0].value;
-  let y = document.getElementsByName('vector_y')[0].value;
-  let z = document.getElementsByName('vector_z')[0].value;
   
   // Create the object variable for storage *TODO(2)
-  let id = `v${objectCount}`
-  let newObject = {id: id, type: "vector"};
+  let newObject = {id: vectorId, type: "vector"};
 
   vector.setAttribute('location', `${x} ${y} ${z}`);
-  vector.setAttribute('id', `${id}`);
+  vector.setAttribute('id', `v${vectorId}`);
   objects.push(newObject);
 
   // Update localStorage
-  localStorage.setItem("objectCount", objectCount + 1);
+  localStorage.setItem("objectCount", vectorId);
   localStorage.setItem("objects", JSON.stringify(objects));
 
   // Add vector to scene
@@ -42,93 +34,46 @@ function addVector() {
 // Adjust an existing object in the scene
 // TODO: Input modes
 //       
-function updateObject(event) {
-  // ID of target (element that called this function)
-  let id = event.target.id;
-  let vector = document.querySelector(`a-vector3#${id}`);
+function updateVectorObject(event, vectorId) {
+  let vector = document.querySelector(`a-vector3#v${vectorId}`);
   
-  let location = vector.getAttribute('location').split(' ');
+  console.log(vector);
   
-  let x = location[0];
-  let y = location[1];
-  let z = location[2];
+  
+  let numbers = event.target.parentNode;
 
-  // Check target name
-  if (event.target.name == "update_x") {
-    x = event.target.value;
-  } else if (event.target.name == "update_y") {
-    y = event.target.value;
-  } else if (event.target.name == "update_z") {
-    z = event.target.value;
-  }
+  let x = numbers.children[0].value;
+  let y = numbers.children[1].value;
+  let z = numbers.children[2].value;
 
   vector.setAttribute('location', `${x} ${y} ${z}`);
 }
 
-function updateDisplay() {
-  // Find display element and clear its contents
-  let display = document.getElementById("display");
-  display.textContent = '';
+function addVectorEntry(event) {
+  let display = document.querySelector(".control-drawer .entries");
+  let template = display.querySelector(".entry-template").firstElementChild;
 
-  // Get localStorage variables
-  let objects = JSON.parse(localStorage.getItem("objects"));
-  let objectCount = JSON.parse(localStorage.getItem("objectCount"));
+  let current_node = event.target.parentNode;
+
+  let vectorId = JSON.parse(localStorage.getItem("objectCount")) + 1;
+
+  let get_x = current_node.querySelector("#x");
+  let get_y = current_node.querySelector("#y");
+  let get_z = current_node.querySelector("#z");
+
+  addVectorObject(vectorId, get_x.value, get_y.value, get_z.value);
+
+  get_x.parentNode.setAttribute("id", `${vectorId}`);
+
+  get_x.setAttribute("onchange", `updateVectorObject(event, ${vectorId});`);
+  get_y.setAttribute("onchange", `updateVectorObject(event, ${vectorId});`);
+  get_z.setAttribute("onchange", `updateVectorObject(event, ${vectorId});`);
+
+  event.target.setAttribute("disabled", "true");
+  event.target.setAttribute("style", "opacity: 0;");
   
-  // Loop through each element in objects
-  objects.forEach(element => {
-    // Create elements for display
-    let container = document.createElement("div");
-    let title = document.createElement("span");
-    let inputX = document.createElement("input");
-    let inputY = document.createElement("input");
-    let inputZ = document.createElement("input");
-
-    let location = document.querySelector(`a-vector3#${element.id}`).getAttribute('location').split(' ');
-
-    // Container
-    container.setAttribute("id", `${element.id}`);
-    container.setAttribute("class", "display-item");
-
-    // Title
-    title.innerHTML = element.id;
-
-    // Input X
-    inputX.setAttribute("type", "number");
-    inputX.setAttribute("name", "update_x");
-    inputX.setAttribute("id", `${element.id}`);
-    inputX.setAttribute("min", "-99");
-    inputX.setAttribute("max", "99");
-    inputX.setAttribute("placeholder", "X");
-    inputX.setAttribute("onchange", "updateObject(event)");
-    inputX.value = location[0];
-
-    // Input Y
-    inputY.setAttribute("type", "number");
-    inputY.setAttribute("name", "update_y");
-    inputY.setAttribute("id", `${element.id}`);
-    inputY.setAttribute("min", "-99");
-    inputY.setAttribute("max", "99");
-    inputY.setAttribute("placeholder", "Y");
-    inputY.setAttribute("onchange", "updateObject(event)");
-    inputY.value = location[1];
-    
-    // Input Z
-    inputZ.setAttribute("type", "number");
-    inputZ.setAttribute("name", "update_z");
-    inputZ.setAttribute("id", `${element.id}`);
-    inputZ.setAttribute("min", "-99");
-    inputZ.setAttribute("max", "99");
-    inputZ.setAttribute("placeholder", "Z");
-    inputZ.setAttribute("onchange", "updateObject(event)");
-    inputZ.value = location[2];
-
-    // Add everything to the display
-    container.appendChild(title);
-    container.appendChild(inputX);
-    container.appendChild(inputY);
-    container.appendChild(inputZ);
-    display.appendChild(container);
-  });
+  let new_node = template.cloneNode(deep=true);
+  display.appendChild(new_node);
 }
 
 function updateVisibility() {
